a42f073611d208a2cdf7a4628facc4b9f2ae1cb5
Shahidha08 <you@example.com>
2025-04-18 13:30:58 +0530

    modified:   personal-finance-manager/src/app/app.component.html 	modified:   personal-finance-manager/src/app/app.module.ts 	new file:   personal-finance-manager/src/app/budget/budget.component.css 	new file:   personal-finance-manager/src/app/budget/budget.component.html 	new file:   personal-finance-manager/src/app/budget/budget.component.spec.ts 	new file:   personal-finance-manager/src/app/budget/budget.component.ts 	new file:   personal-finance-manager/src/app/dashboard/dashboard.component.css 	new file:   personal-finance-manager/src/app/dashboard/dashboard.component.html 	new file:   personal-finance-manager/src/app/dashboard/dashboard.component.spec.ts 	new file:   personal-finance-manager/src/app/dashboard/dashboard.component.ts 	new file:   personal-finance-manager/src/app/expense-form/expense-form.component.css 	new file:   personal-finance-manager/src/app/expense-form/expense-form.component.html 	new file:   personal-finance-manager/src/app/expense-form/expense-form.component.spec.ts 	new file:   personal-finance-manager/src/app/expense-form/expense-form.component.ts 	new file:   personal-finance-manager/src/app/expense-list/expense-list.component.css 	new file:   personal-finance-manager/src/app/expense-list/expense-list.component.html 	new file:   personal-finance-manager/src/app/expense-list/expense-list.component.spec.ts 	new file:   personal-finance-manager/src/app/expense-list/expense-list.component.ts

--- personal-finance-manager/src/app/app.module.ts
+++ personal-finance-manager/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ExpenseListComponent } from './expense-list/expense-list.component';
+import { ExpenseFormComponent } from './expense-form/expense-form.component';
+import { BudgetComponent } from './budget/budget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ExpenseListComponent,
+    ExpenseFormComponent,
+    BudgetComponent
   ],
   imports: [
     BrowserModule,
